f887c37e8b0d36033a30565fe3c1b4943000a184
KSodre <sodre.krissie@gmail.com>
2023-05-23 10:41:55 -0300

    v1.0

---
+++ anotacao.mk
@@ -0,0 +1,8 @@
+Anotação de reunião
+
+Trilhas em HTML:
+Tem macro!! (Como que é feito? Qual a linguagem?) jsxbin
+
+InDesign > script > html > macro > scorm
+
+DIV de capítulo ta multiplicando os IDs 
